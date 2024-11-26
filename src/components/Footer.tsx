53de4bd2673f141b8ff73c334cfc626b4f6b6d85
BARONYXZ <krumalmarek@gmail.com>
2024-11-26 16:49:04 +0100

    Responzivita

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,21 +9,26 @@ export default function Footer() {
       <main className="flex-grow"></main>
       <footer className="text-center py-8 mt-auto text-sm font-normal">
         <nav className="max-w-3xl mx-auto flex flex-col items-center justify-center">
-          <div>© 2024 Marek Křůmal | Olomouc Česká Republika</div>
-          <div className="flex items-center justify-center py-3">
-            {t("createdWith")}{" "}
-            <Heart
-              strokeWidth={3}
-              className="inline size-4 text-red-400 hover:text-pink-500 mx-1 transition-transform duration-500 ease-in-out hover:rotate-12 hover:scale-125"
-            />
-            {t("usingTechnologies")}{" "}
-            <Link
-              href="https://github.com/marekkrumal"
-              className="ml-1 underline text-cyan-600 hover:text-cyan-400 font-semibold transition-colors duration-300"
-            >
-              {t("github")}
-            </Link>
-            .
+          <div className="text-gray-300">
+            © 2024 Marek Křůmal | Olomouc, Česká Republika
+          </div>
+          <div className="flex flex-col sm:flex-row items-center justify-center py-3 space-y-2 sm:space-y-0 sm:space-x-3">
+            <div>
+              {t("createdWith")}{" "}
+              <Heart
+                strokeWidth={3}
+                className="inline text-red-400 hover:text-pink-500 mx-1 transition-transform duration-500 ease-in-out hover:rotate-12 hover:scale-125"
+              />
+              {t("usingTechnologies")}
+            </div>
+            <div>
+              <Link
+                href="https://github.com/marekkrumal"
+                className="underline text-cyan-600 hover:text-cyan-400 font-semibold transition-colors duration-300"
+              >
+                {t("github")}
+              </Link>
+            </div>
           </div>
         </nav>
       </footer>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,35 +3,74 @@ import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 import { useTranslations, useLocale } from "next-intl";
 import LanguageButton from "./LanguageButton";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 export default function Navbar() {
   const t = useTranslations("NavbarLinks");
   const locale = useLocale();
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
 
   return (
     <header className="sticky top-0 bg-background">
       <div className="max-w-7xl mx-auto flex justify-between items-center gap-3 px-4 pt-8 pb-4">
-        {/* Vlevo - Marek Křůmal */}
-        <div className="flex-shrink-0">
+        <div className="flex-shrink-0 hidden md:flex">
           <Link href="/" className="font-bold text-xl uppercase">
             {t("marekkrumal")}
           </Link>
         </div>
 
-        {/* Uprostřed - Navigační odkazy */}
-        <nav className="flex space-x-10 uppercase font-semibold mx-auto text-center">
+        <nav className="space-x-10 uppercase font-semibold mx-auto hidden md:flex text-center">
           <Link href={`/${locale}/projects`}>{t("projects")}</Link>
           <Link href={`/${locale}/about`}>{t("about")}</Link>
           <Link href={`/${locale}/posts`}>{t("posts")}</Link>
           <Link href={`/${locale}/contact`}>{t("contact")}</Link>
         </nav>
 
-        {/* Vpravo - Tlačítka pro téma a jazyk */}
         <div className="flex items-center space-x-3">
           <ThemeToggle />
           <LanguageButton />
         </div>
+
+        <div className="block md:hidden p-1 z-50" onClick={handleClick}>
+          {click ? (
+            <X size={40} strokeWidth={1} className="text-white text-3xl z-50" />
+          ) : (
+            <Menu
+              size={40}
+              strokeWidth={1}
+              className="text-white text-3xl z-50"
+            />
+          )}
+        </div>
       </div>
+      <ul
+        className={`${
+          click ? "block" : "hidden"
+        } absolute top-0 left-0 md:hidden w-full h-screen bg-background text-white flex flex-col text-xl justify-center font-bold uppercase items-center z-40`}
+      >
+        <li className="p-[2rem]">
+          <Link href="/">{t("marekkrumal")}</Link>
+        </li>
+        <hr className="border-t border-gray-600 w-3/4 mx-auto my-2" />
+        <li className="p-[2rem]">
+          <Link href={`/${locale}/projects`}>{t("projects")}</Link>
+        </li>
+        <hr className="border-t border-gray-600 w-3/4 mx-auto my-2" />
+        <li className="p-[2rem]">
+          <Link href={`/${locale}/about`}>{t("about")}</Link>
+        </li>
+        <hr className="border-t border-gray-600 w-3/4 mx-auto my-2" />
+        <li className="p-[2rem]">
+          <Link href={`/${locale}/posts`}>{t("posts")}</Link>
+        </li>
+        <hr className="border-t border-gray-600 w-3/4 mx-auto my-2" />
+        <li className="p-[2rem]">
+          <Link href={`/${locale}/contact`}>{t("contact")}</Link>
+        </li>
+      </ul>
     </header>
   );
 }
