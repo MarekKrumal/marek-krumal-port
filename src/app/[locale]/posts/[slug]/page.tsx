6a856b77b022b644228d9b12777be9670bbe7322
BARONYXZ <krumalmarek@gmail.com>
2025-01-14 20:21:22 +0100

    test

--- src/app/[locale]/posts/[slug]/page.tsx
+++ src/app/[locale]/posts/[slug]/page.tsx
@@ -1,33 +1,3 @@
-import { notFound } from "next/navigation";
-import { allPosts } from "../PostsData";
-
-export async function generateStaticParams() {
-  return allPosts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-export default function BlogPostDetail({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } = params;
-
-  const post = allPosts.find((p) => p.slug === slug);
-
-  if (!post) {
-    notFound();
-  }
-
-  return (
-    <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
-      <div className="text-sm text-gray-500 mb-4">{post.date}</div>
-      <div
-        className="prose dark:prose-invert"
-        dangerouslySetInnerHTML={{ __html: post.content }}
-      />
-    </div>
-  );
+export default function Page() {
+  return <div>Post</div>;
 }
