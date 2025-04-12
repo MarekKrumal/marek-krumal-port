c0370e0af0278c3ce1511a3c50fc622cb316a295
MarekKrumal <krumalmarek@gmail.com>
2025-04-12 11:25:42 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 const isDev = process.argv.indexOf("dev") !== -1;
 const isBuild = process.argv.indexOf("build") !== -1;
 
-// Spuštění Velite při vývoji nebo při buildování
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = "1";
-  import("velite").then((m) => m.build({ watch: isDev, clean: !isDev }));
-}
-
 // Konfigurace pro Next.js
 const withNextIntl = createNextIntlPlugin();
 const withAnalyzer = withBundleAnalyzer({
