0bbca7df7d07560e9b9f0bdf3d8a76ba0588b413
MarekKrumal <krumalmarek@gmail.com>
2025-04-18 08:41:37 +0200

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import withBundleAnalyzer from "@next/bundle-analyzer";
-import rehypeAutoLinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
 
 const withNextIntl = createNextIntlPlugin();
 const withAnalyzer = withBundleAnalyzer({
@@ -17,14 +14,6 @@ const nextConfig: NextConfig = {
 
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypePrettyCode, { theme: "andromeeda" }],
-      rehypeAutoLinkHeadings,
-    ],
-  },
 });
 
 export default withAnalyzer(withNextIntl(nextConfig));
